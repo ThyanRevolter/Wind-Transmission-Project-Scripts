82f1c1e31ed7d1f4fb1bad55133ad34d2c7b2a12
Adhithyan Sakthivelu <65640341+ThyanRevolter@users.noreply.github.com>
2021-07-19 20:05:42 +0530

    Create process_shp_file.py
    without  polygonize function

---
+++ process_shp_file.py
@@ -0,0 +1,42 @@
+import os
+import processing
+import qgis.core
+outputFolder = r'D:\Adhithyan Data\Wind Transmission Project\CA\Filtered'
+state_Name = 'CA'
+
+for file in os.listdir(outputFolder):
+  if file.endswith('.shp'):
+    vector_file_path = outputFolder +'/' + file
+    vlayer = QgsVectorLayer(vector_file_path, file, "ogr")
+    zero_dn = []
+    count = 0
+    for feature in vlayer.getFeatures():
+      if feature['DN'] != 1:
+        zero_dn.append(feature.id())
+      else:
+        count +=1
+    res = vlayer.dataProvider().deleteFeatures(zero_dn)
+    # Removing the file as there are no corresponding Area    
+    print(res)
+    print(count)
+    vlayer.commitChanges()
+    vlayer.dataProvider().addAttributes([QgsField("State_Abbr", QVariant.String),
+                                        QgsField("Area", QVariant.Double, '', 10, 3)])    
+    vlayer.updateFields()
+    area_expression = QgsExpression('area($geometry)')
+    context = QgsExpressionContext()
+    context.appendScopes(QgsExpressionContextUtils.globalProjectLayerScopes(vlayer))
+    with edit(vlayer):
+      for feature in vlayer.getFeatures():
+        context.setFeature(feature)
+        feature['State_Abbr'] = state_Name
+        feature['Area'] = area_expression.evaluate(context)
+        vlayer.updateFeature(feature)
+    vlayer.dataProvider().deleteAttributes([vlayer.fields().indexFromName('DN')])
+    vlayer.updateFields()
+    del vlayer
+    if count == 0:
+      for delete_file in os.listdir(outputFolder):
+        if os.path.splitext(delete_file)[0] == os.path.splitext(file)[0]:
+          os.remove(outputFolder +'/' + delete_file)
+
